eca70f54b7bf638a5af9c398314434b13614632c
Lani Luong <loanlu@gmail.com>
2017-04-17 18:05:26 +0000

    moderator added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
 before_action :require_sign_in, except: :show
-before_action :authorize_user, except: [:show, :new, :create]
+before_action :authorize_user, only: [:destroy]
+before_action :authorize_moderator, only: [:edit, :update]
+
   def show
     @post = Post.find(params[:id])
   end
@@ -66,4 +68,13 @@ before_action :authorize_user, except: [:show, :new, :create]
       redirect_to [post.topic, post]
     end
   end
+  
+  def authorize_moderator
+    post = Post.find(params[:id])
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to [post.topic, post]
+    end
+  end
+
 end
--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -3,4 +3,7 @@ module TopicsHelper
        current_user && current_user.admin?
    end
 
+   def user_is_moderator?
+      current_user && current_user.moderator?
+   end
 end
