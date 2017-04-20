6b6d60d92c6db209e507d0a5b7c6c97ee55d580e
Lani Luong <loanlu@gmail.com>
2017-04-20 22:15:34 +0000

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@
   resources :topics do
    resources :posts, except: [:index]
   end
+  
+  resources :posts, only: [] do
+   resources :comments, only: [:create, :destroy]
+  end
+  
   resources :users, only: [:new, :create]
   resources :questions
   resources :sessions, only: [:new, :create, :destroy]
