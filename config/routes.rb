69aa1ed0da5238184da9c6b04ee748d4de8b29ea
Lani Luong <loanlu@gmail.com>
2017-04-25 19:40:12 +0000

    factories and refactor tests

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@
    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
   
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :questions
   resources :sessions, only: [:new, :create, :destroy]
   
